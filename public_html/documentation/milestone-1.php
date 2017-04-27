8c848586cfdf1dea79a4908b6d2ab6269666a524
Jabari Farrar <jofarrar@gmail.com>
2017-04-26 21:19:34 -0600

    updated the html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,17 +27,25 @@
 			<p>Age: 56 years old</p>
 			<p>Education: M.S. in human resources</p>
 			<p>Technology: smartphone (Samsung S8), laptop (Mac at home), and a personcal computer at work (Dell) </p>
+			<p>Attitudes and Behaviors: Lester in short on time and the only HR representative for this company of 15 people.
+				Lester is an avid user of LinkedIn and likes to look at the links included on a person's LinkedIn page if the candidate
+				is a potential match for the positon.</p>
 			<p>Frustrations: Lester needs to hire 2-3 people for web applications positions for a scientific software comapny;
 				however, these people need to be able understand not only web development tools, but they need to have a
 				technical backgoround to understand the content of the web applications. He must hire the proper candidates within
 				two weeks so they have time ramp up before the new projects are kicked off.</p>
 			<p>Goal: Since the company is composed of a total of 15 employees, Lester is burdened with hiring for other open
 				positions within the company as well. Lester has very little time to make a decision as to whether a potential
-				candidate is a fit or not. Lester must get this informtion from quick glance at a site or he moves on.</p>
+				candidate is a fit or not. Lester must get this informtion from a quick glance at a site or he moves on.</p>
 			<p>User Story: As a user I need to quickly access the candiates web developmenet skills as well as their ability
 				 understand the technical content of the web applications they will be building.</p>
 
-			<p><strong>Use Case</strong></p>
+			<p><strong>Use Case:</strong></p>
+				<p>Lester in perusing through LinkedIn through his Mozilla browser at 7:45 am after searching for local candidates using a few keywords.
+				He knows he has to present a few candidates to the hiring manager by 1:00 pm today in spite of his meetings
+				this morning form 10:00 am to 11:30 am, and has no time to waste.</p>
+				<p> Lester has viewed my LinkedIn page and sees a profile with a strong background in chemistry and web development experience
+				His interest is piqued and he views the Github and personal website links</p>
 
 
 
