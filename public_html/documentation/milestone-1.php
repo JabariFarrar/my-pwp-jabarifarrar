ae1b8d471a0a3149f32a9cd5aa6b659083946dd7
Jabari Farrar <jofarrar@gmail.com>
2017-04-26 20:22:10 -0600

    added html tags and verbage

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE htmlL>
+<html>
+	<head>
+
+		<meta charset="UTF-8">
+
+		<title>PWP Milestone One</title>
+
+	</head>
+
+	<body>
+
+		<header>
+
+			<h1> PWP Milestone One</h1>
+
+		</header>
+
+			<p><strong>Purpose:</strong> I would like my website to be an online version of my resume to help secure clients, an internship or employment.</p>
+
+			<p><strong>Audience:</strong>The audience for this website are potential employers and clients for freelance work.</p>
+
+			<p><strong>Goal:</strong> The goal of this website is to acquire an internship, freelance work or employment.</p>
+
+			<p><strong>Persona</strong></p>
+			<p>Name:Lester Boomhackel</p>
+			<p>Age: 56 years old</p>
+			<p>Education: M.S. in human resources</p>
+			<p>Technology: smartphone (Samsung S8), laptop (Mac at home), and a personcal computer at work (Dell) </p>
+			<p>Frustrations: Lester needs to hire 2-3 people for web applications positions for a scientific software comapny;
+				however, these people need to be able understand not only web development tools, but they need to have a
+				technical backgoround to understand the content of the web applications. He must hire the proper candidates within
+				two weeks so they have time ramp up before the new projects are kicked off.</p>
+			<p>Goal: Since the company is composed of a total of 15 employees, Lester is burdened with hiring for other open
+				positions within the company as well. Lester has very little time to make a decision as to whether a potential
+				candidate is a fit or not. Lester must get this informtion from quick glance at a site or he moves on.</p>
+			<p>User Story: As a user I need to quickly access the candiates web developmenet skills as well as their ability
+				 understand the technical content of the web applications they will be building.</p>
+
+			<p><strong>Use Case</strong></p>
+
+
+
+
+
+
+
+
+	</body>
+
+
+
+
+
+</html>
+
